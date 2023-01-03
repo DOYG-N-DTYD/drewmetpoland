e74fd42caa0840128b9aff9929aac38ad9c9c0d5
DOYG-N-DTYD <Miroslawzborowski1105@gmail.com>
2023-01-03 14:52:04 +0100

    begin load empty project after spring.start.io

---
+++ src/test/java/com/website/drewmetpoland/DrewmetpolandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.drewmetpoland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrewmetpolandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
